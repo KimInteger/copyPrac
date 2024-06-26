c643607fbcf3f11a2b79a63cbd1ee8f26894fed1
KimInteger <kjs20151240@gmail.com>
2024-06-26 10:51:55 +0900

    test: shallowCopy test

--- test.js
+++ test.js
@@ -1,1 +1,32 @@
-console.log(test.js);
+const a = 1;
+
+const b = 1;
+
+console.log(a === b);
+
+const c = [1]; // ? 변수 C의 index 0번째는 값이 1이다.
+
+const d = [1];
+
+console.log( c[0] === d[0] );
+
+console.log( c === d ); // print : false;
+
+const origin = [
+  {name : '김정수', age : 29},
+  {name : '김인티저', age : 27}
+];
+
+const shallow = origin.map((item)=>{
+  return item;
+})
+
+console.log(origin);
+
+console.log(shallow);
+
+shallow[1].name = '인티저 킴';
+
+console.log(origin);
+
+console.log(shallow);
